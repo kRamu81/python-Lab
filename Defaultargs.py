57b7dd895de7167606629a453ff900ed0834a6dd
kRamu581 <kanamramu18@gmail.com>
2024-10-28 20:10:12 +0530

    Defaultargs.py

---
+++ Defaultargs.py
@@ -0,0 +1,7 @@
+#write a program using the function default arguments
+
+#using the defalut condition with keyword with arguments
+def introduce (name,age,place):
+    print(f"my name is{name},age is{age},from{place}")
+
+introduce ("ramu","18","kadapa")
