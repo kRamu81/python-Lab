337a8633c55b7ee01752765f1924429397abb40a
kRamu581 <kanamramu18@gmail.com>
2024-10-28 20:11:03 +0530

    largest among 3 no.py

---
+++ largest among 3 no.py
@@ -0,0 +1,8 @@
+#biggest among three no using list and max condition
+
+num=int(input("enter the first value?"))
+num2=int(input("enter the second value?"))
+num3=int(input("enter the third value?"))
+
+list =(num,num2,num3)
+print(max(list))
